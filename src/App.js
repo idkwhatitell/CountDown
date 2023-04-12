51529a16c8a7984ecb7be035da74042109251e0b
Kaan Cömert <comert9806@gmail.com>
2023-04-12 23:17:32 +0300

    last

--- src/App.js
+++ src/App.js
@@ -1,43 +1,88 @@
-import React, {useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
+
 const App = () => {
+  const meetings = [
+    {
+      name: "Delta Package",
+      day: 2,
+      time: "13:30:00"
+    },
+    {
+      name: "Master Data Meeting",
+      day: 2,
+      time: "10:00:00"
+    },
+    {
+      name: "General implementation-issues",
+      day: 3,
+      time: "15:00:00"
+    },
+    {
+      name: "General Test Meeting",
+      day: 4,
+      time: "15:00:00"
+    }
+  ];
+
+  const [currentMeetingIndex, setCurrentMeetingIndex] = useState(0);
   const [timerDays, setTimerDays] = useState('00');
   const [timerHours, setTimerHours] = useState('00');
   const [timerMinutes, setTimerMinutes] = useState('00');
   const [timerSeconds, setTimerSeconds] = useState('00');
+  const [meetingName, setMeetingName] = useState(meetings[currentMeetingIndex].name);
 
   let interval = useRef();
 
+  const getNextMeetingIndex = (currentMeetingIndex) => {
+    let nextMeetingIndex = currentMeetingIndex + 1;
+    if (nextMeetingIndex >= meetings.length) {
+      nextMeetingIndex = 0;
+    }
+    return nextMeetingIndex;
+  }
+
   const startTimer = () => {
-    const countdownDate = new Date('Feb 21 2023 15:00:00').getTime();
-    interval = setInterval(() => {
-      const now = new Date ().getTime();
-      const distance = countdownDate - now;
+    let meetingTime = new Date().getTime();
+    interval.current = setInterval(() => {
+      const currentDate = new Date();
+      const currentDay = new Date().getDay();
+
+      const currentMeeting = meetings[currentMeetingIndex];
+      if (currentMeeting.day !== currentDay) {
+        setCurrentMeetingIndex(getNextMeetingIndex(currentMeetingIndex));
+        setMeetingName(meetings[getNextMeetingIndex(currentMeetingIndex)].name);
+        return;
+      }
+
+      meetingTime = new Date(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()} ${currentMeeting.time}`).getTime();
+      const now = currentDate.getTime();
+      const distance = meetingTime - now;
+
+      if (distance < 0) {
+        setCurrentMeetingIndex(getNextMeetingIndex(currentMeetingIndex));
+        setMeetingName(meetings[getNextMeetingIndex(currentMeetingIndex)].name);
+        return;
+      }
 
       const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      if (distance < 0) {
-        //stop our timer
-        clearInterval(interval.current);
-      } else {
-        //update timer
-        setTimerDays(days);
-        setTimerHours(hours);
-        setTimerMinutes(minutes);
-        setTimerSeconds(seconds);
-      }
-
+      // update timer
+      setTimerDays(days < 10 ? `0${days}` : days);
+      setTimerHours(hours < 10 ? `0${hours}` : hours);
+      setTimerMinutes(minutes < 10 ? `0${minutes}` : minutes);
+      setTimerSeconds(seconds < 10 ? `0${seconds}` : seconds);
     }, 1000);
   };
 
-  //componentDidMount
+  // componentDidMount
   useEffect(() => {
     startTimer();
-    return () =>{
+    return () => {
       clearInterval(interval.current);
     };
   });
@@ -48,7 +93,7 @@ const App = () => {
         <div>
           <span className ="mdi mdi-calendar-clock timer-icon"></span>
           <h2>Countdown for Meeting</h2>
-          <p>Time left for meeting</p>
+          <p>{meetingName}</p>
         </div>
         <div>
           <section>
